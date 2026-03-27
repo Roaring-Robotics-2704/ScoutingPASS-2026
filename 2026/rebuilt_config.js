57cd600befb3b79a5ac5c90daf9f81841f762279
fuddster <fuddster@users.noreply.github.com>
2026-03-27 08:34:23 -0500

    Update rebuilt_config.js

--- 2026/rebuilt_config.js
+++ 2026/rebuilt_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2026wiply",
+      "defaultValue": "2026ilch",
       "required": "true"
     },
     { "name": "Match Level",
